dec141b13b5383e77742a824f67d763e79db175a
FIXED-TERM Wilczynski Krzysztof (DCPL/PJ-i4.0) <krzysztofwilczynski@mail.com>
2019-03-07 16:57:30 +0100

    Added style changing button.

--- Angular/forms_app/src/app/home/home.component.ts
+++ Angular/forms_app/src/app/home/home.component.ts
@@ -7,38 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   firstClick() {
-    console.log('clicked');
+    this.h1Style = ! this.h1Style;
+    console.log('clicked, style changed');
   }
-
-//   Other events to bind:
-//     (focus)="myMethod()"
-//     (blur)="myMethod()" 
-//     (submit)="myMethod()"  
-//     (scroll)="myMethod()"
-
-//     (cut)="myMethod()"
-//     (copy)="myMethod()"
-//     (paste)="myMethod()"
-
-//     (keydown)="myMethod()"
-//     (keypress)="myMethod()"
-//     (keyup)="myMethod()"
-
-//     (mouseenter)="myMethod()"
-//     (mousedown)="myMethod()"
-//     (mouseup)="myMethod()"
-
-//     (click)="myMethod()"
-//     (dblclick)="myMethod()"
-
-//     (drag)="myMethod()"
-//     (dragover)="myMethod()"
-//     (drop)="myMethod()"
 
 }
